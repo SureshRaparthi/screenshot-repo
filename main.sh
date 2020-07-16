cb74bd34abd9e56acf35c14fb6f32a4fdaa9d4e5
SureshRaparthi <34570280+SureshRaparthi@users.noreply.github.com>
2020-07-16 21:17:57 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -14,21 +14,25 @@ cd $dir
 cd Firefox
 docker build -t firefoximg .
 
-docker run -d  -p 4050 chromeimg
-docker run -d -p 4051 firefoximg
+docker run -d  -p 4444:4444 chromeimg
+docker run -d -p 4445:4445 firefoximg
 
 cd $dir
 
-python screenshot.py chrome {inputurl}
+sleep 20
 
-python screenshot.py firefox {inputurl}
+python3 screenshot.py chrome $inputurl
+
+#sleep 20
+
+#python3 screenshot.py firefox $inputurl
 
 # please update your s3 bucket details here
 
 aws s3 cp "chrome_screenshot.png" "s3://bucket2test123/chrome_screenshot.png"
-
+sleep 10
 aws s3 presign s3://bucket2test123/chrome_screenshot.png --expires-in 1800
 
-aws s3 cp "firefox_screenshot.png" "s3://bucket2test123/firefox_screenshot.png"
+#aws s3 cp "firefox_screenshot.png" "s3://bucket2test123/firefox_screenshot.png"
 
-aws s3 presign s3://bucket2test123/firefox_screenshot.png --expires-in 1800
+#aws s3 presign s3://bucket2test123/firefox_screenshot.png --expires-in 1800
