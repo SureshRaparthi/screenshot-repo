c060cdb3a5d3863fa15afa25ad9bdc5770f6cd36
SureshRaparthi <34570280+SureshRaparthi@users.noreply.github.com>
2020-07-16 14:42:28 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -25,8 +25,8 @@ python screenshot.py firefox {inputurl}
 
 aws s3 cp "chrome_screenshot.png" "s3://bucket2test123/chrome_screenshot.png" --region=ap-south-1 --output=json
 
-aws s3 presign s3://test-bucket/chrome_screenshot.png --expires-in 1800
+aws s3 presign s3://bucket2test123/chrome_screenshot.png --expires-in 1800
 
 aws s3 cp "firefox_screenshot.png" "s3://bucket2test123/firefox_screenshot.png" --region=ap-south-1 --output=json
 
-aws s3 presign s3://test-bucket/firefox_screenshot.png --expires-in 1800
+aws s3 presign s3://bucket2test123/firefox_screenshot.png --expires-in 1800
